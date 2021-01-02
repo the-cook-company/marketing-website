b4d2c1ab22afbfb086d5c008baa9fac8436b85f7
Forrest Keller <forrestblackburnkeller@gmail.com>
2021-01-02 01:51:24 -0700

    Actually fix fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import { Box, ChakraProvider } from "@chakra-ui/react";
-import { Global } from "@emotion/react";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import { FunctionComponent } from "react";
@@ -11,12 +10,14 @@ const CustomApp: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
       <Head>
         <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
       </Head>
-      <Global
-        styles={`@import url('https://fonts.googleapis.com/css2?family=Hind:wght@700&family=Roboto+Slab&display=swap');`}
-      />
       <Box minHeight="100vh" bg="white.50">
         <Component {...pageProps} />
       </Box>
+      <style jsx global>
+        {`
+          @import url("https://fonts.googleapis.com/css2?family=Hind:wght@700&family=Roboto+Slab&display=swap");
+        `}
+      </style>
     </ChakraProvider>
   );
 };
